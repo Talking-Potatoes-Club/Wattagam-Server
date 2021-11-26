88730f1d680c32e4c34e10c179dd7a69a67944cd
RulLu16 <may05200@naver.com>
2021-11-26 14:37:40 +0900

    [fix] 프로필 이미지 경로 설정

--- wattagam/wattagam/settings.py
+++ wattagam/wattagam/settings.py
@@ -145,7 +145,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
